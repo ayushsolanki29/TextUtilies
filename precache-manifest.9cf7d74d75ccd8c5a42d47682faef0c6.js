752def63be0f431ee33b9e5b3bd5128f83d2c3cf
Ayush solanki <103575160+ayushsolanki29@users.noreply.github.com>
2022-12-21 19:19:44 +0530

    Updates

---
+++ precache-manifest.9cf7d74d75ccd8c5a42d47682faef0c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fead7ba4afb7ab0dfefb",
+    "url": "/TextUtilies/static/css/main.131353ec.chunk.css"
+  },
+  {
+    "revision": "fead7ba4afb7ab0dfefb",
+    "url": "/TextUtilies/static/js/main.fead7ba4.chunk.js"
+  },
+  {
+    "revision": "36f5eeb1ced8a33d2704",
+    "url": "/TextUtilies/static/js/1.36f5eeb1.chunk.js"
+  },
+  {
+    "revision": "8ba146803b41212359a8",
+    "url": "/TextUtilies/static/js/2.8ba14680.chunk.js"
+  },
+  {
+    "revision": "0ec4f0c01427dcb5c43b",
+    "url": "/TextUtilies/static/js/3.0ec4f0c0.chunk.js"
+  },
+  {
+    "revision": "0555e5c2d6eb8643c2e3",
+    "url": "/TextUtilies/static/js/4.0555e5c2.chunk.js"
+  },
+  {
+    "revision": "5071b721312903efa354",
+    "url": "/TextUtilies/static/js/5.5071b721.chunk.js"
+  },
+  {
+    "revision": "211ac692690fc6df0e57",
+    "url": "/TextUtilies/static/js/runtime~main.211ac692.js"
+  },
+  {
+    "revision": "e564c97a4e0161a19e4d67111db14a2f",
+    "url": "/TextUtilies/index.html"
+  }
+];
