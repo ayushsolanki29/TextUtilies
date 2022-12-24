4dce11f440f100fb482e77bf5470d964df74c561
Ayush Solanki <103575160+ayushsolanki29@users.noreply.github.com>
2022-12-24 18:41:58 +0530

    Add files via upload

---
+++ precache-manifest.8311b425b56b51a803c28a336d796986.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e758beeadff1eb2b5e7",
+    "url": "/static/css/main.93d59074.chunk.css"
+  },
+  {
+    "revision": "8e758beeadff1eb2b5e7",
+    "url": "/static/js/main.8e758bee.chunk.js"
+  },
+  {
+    "revision": "cfefcbac11a72e123d3d",
+    "url": "/static/js/1.cfefcbac.chunk.js"
+  },
+  {
+    "revision": "a171a9d0e8b5df7fbf76",
+    "url": "/static/js/2.a171a9d0.chunk.js"
+  },
+  {
+    "revision": "d6c4e3c2feb441f8631f",
+    "url": "/static/js/3.d6c4e3c2.chunk.js"
+  },
+  {
+    "revision": "3796ea852fd0d43d26ee",
+    "url": "/static/js/4.3796ea85.chunk.js"
+  },
+  {
+    "revision": "13372a70055758d3225c",
+    "url": "/static/js/5.13372a70.chunk.js"
+  },
+  {
+    "revision": "f661a9545b2b453d6547",
+    "url": "/static/js/runtime~main.f661a954.js"
+  },
+  {
+    "revision": "1fc91ed88d04f364a19c91c293b1e318",
+    "url": "/index.html"
+  }
+];
